e496783cf5fe717d1236d9a087cecb8d1ddd6489
oryair <oryair36@gmail.com>
2017-11-29 13:59:32 +0200

    Edit Matlab files Add Riemannian presentation

--- pca_2.m
+++ pca_2.m
@@ -1,46 +1,55 @@
 %% Clear
-clear;clc;close all;
+close all;
+clear;
+
 %% Import data
 subjects=[203,204,206,207,208,210,211,212,213,214,215,226,229,230,231,...
           232,239,242,244,245,248,249,250];
 num_subj=length(subjects);
 len=10*300; %Length of input vectors
-inputdata=zeros(len,num_subj);
+mX=zeros(len,num_subj);
 for ii=1:num_subj
     in=load(strcat(num2str(subjects(ii)),'_2_proc.mat'),'findata'); %Load mat file
-    inputdata(:,ii)=in.findata;
+    mX(:,ii)=in.findata;
 end
 clear in ii
 
 %% PCA preprocessing
 % Set mean to 0
-mean_vec=mean(inputdata,2);
-inputdata=inputdata-kron(mean_vec,ones(1,num_subj));
+% mX    = inputdata;
+mX    = mData;
+vMean = mean(mX, 2);
+mX    = mX - vMean;
+
 % Set variance to 1
-var_vec=mean(inputdata.^2,2);
-inputdata=inputdata./kron(sqrt(var_vec),ones(1,num_subj));
+vStd = std(mX, [], 2);
+mX   = mX ./ vStd;
 
 %% Actual PCA
-cov_mat=zeros(len);
-for ii=1:num_subj
-    cov_mat=cov_mat+(1/num_subj)*inputdata(:,ii)*inputdata(:,ii)';
-end
+mCov = mX * mX';
+% cov_mat=zeros(len);
+% for ii=1:num_subj
+%     cov_mat=cov_mat+(1/num_subj)*mX(:,ii)*mX(:,ii)';
+% end
 %%
-[V,D]=eig(cov_mat);
+[V, D] = eig(mCov);
 %%
-y=V'*inputdata;
+mY = V' * mX;
 %%
-plot(abs(y));
+plot(abs(mY));
 %%
 real_data=[23,14,33,24,26,29,31,21,21,25,21,24,27,19,24,27,22,16,22,20,13,20,33];
-scatter(real_data,y(1,:));
+scatter(real_data,mY(1,:));
+
+%%
+figure; scatter3(mY(1,:), mY(2,:), mY(3,:), 100, real_data, 'Fill'); colorbar;
 
 %% Kernel PCA
 eps=1e2;
 K_mat=zeros(num_subj);
 for ii=1:num_subj
     for jj=1:ii
-        K_mat(ii,jj)=exp(norm(inputdata(:,ii)-inputdata(:,jj))/eps);
+        K_mat(ii,jj)=exp(norm(mX(:,ii)-mX(:,jj))/eps);
         K_mat(jj,ii)=K_mat(ii,jj);
     end
 end
--- preprocess.m
+++ preprocess.m
@@ -1,15 +1,44 @@
-subjects=[203,204,206,207,208,210,211,212,213,214,215,226,229,230,231,...
-          232,239,242,244,245,248,249,250];
-cutoff=300; % No point in saving empty frequencies
-num_of_elctd=10; % Num of electrodes to use
-for ii=subjects
-    load(strcat(num2str(ii),'_2.mat')); %Load mat file
-    fftdata=fft(data,[],2); %fft on the time component
-    fftdata=abs(fftdata); %This will cancel time-shift effects
-    fftdata=permute(fftdata,[3,2,1]);
-    meandata=mean(fftdata); %Average on all experiments for one electrode
-    meandata=permute(meandata,[3,2,1]);
-    cutdata=meandata(1:num_of_elctd,1:cutoff);
-    findata=cutdata(:); %Straighten to one vector
-    save(strcat('processed/',num2str(ii),'_2_proc.mat'),'findata');
+close all
+clear
+
+dirPath = 'C:\Users\Oryair\Desktop\Workarea\BrainSway\';
+XlsFile = xlsread([dirPath, 'clinicalHDRS-2.xlsx']);
+
+
+%%
+vSubjectIdx = XlsFile(:,1);
+        
+cutoff        = 400; % No point in saving empty frequencies
+num_of_elctd  = 10;  % Num of electrodes to use
+vElectordeIdx = randperm(62, num_of_elctd);  % Num of electrodes to use
+D             = cutoff * num_of_elctd;
+Ns            = length(vSubjectIdx);
+
+mData = nan(D, 0);
+for ii = 1 : Ns
+% for ii = 1 : 1
+    ii
+    subject  = vSubjectIdx(ii);
+    fileName = ['LICI_CSD-mat\session 2\', num2str(subject), '_2.mat'];
+    
+    if ~(exist([dirPath, fileName], 'file') == 2)
+        continue;
+    end
+    load([dirPath, fileName]);
+    
+    mX      = data;
+    mXF     = fft(mX, [], 2);
+    mXfAbs  = abs(mXF); %This will cancel time-shift effects
+    mXfMean = mean(mXfAbs, 3);
+    
+    mDatai  = mXfMean(vElectordeIdx,1:cutoff);
+
+%     figure; plot( fftshift(mXfAbs(1:10,:,1)') ); ax(1) = gca;
+%     figure; plot( fftshift(mXfMean(1:10,:))' );  ax(2) = gca;
+%     linkaxes(ax, 'xy');
+    
+    mData(:,end+1) = mDatai(:);
+%     cutdata = meandata(1:num_of_elctd,1:cutoff);
+%     findata=cutdata(:); %Straighten to one vector
+%     save(strcat('processed/',num2str(ii),'_2_proc.mat'),'findata');
 end
