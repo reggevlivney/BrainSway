8f3c1686ce90b1ffbfa64a07d906add37427442b
reggevlivney <reggevlivney@campus.technion.ac.il>
2018-01-02 18:11:42 +0200

    Added projection to 2d cov subspace

--- CovarianceRandElecs.m
+++ CovarianceRandElecs.m
@@ -12,18 +12,19 @@ XlsFile = xlsread([dirPath, 'clinicalHDRS-2.xlsx']);
 %% Preprocess data
 vSubjectIdx = XlsFile(:,1);
         
-Nelc               = 50;  % Num of electrodes
-vSessions          = 5;
+Nelc               = 40;  % Num of electrodes
+vSessions          = 2:5;
 vElectordeIdx      = sort(randperm(62, Nelc));  % Pick random electrodes
 Ns                 = length(vSubjectIdx); %Number of subjects
 vSubjectsInSession = vSubjectIdx;
 
 %% Create Cov matricies and Riemann averages
-% mData = nan(D, 0);
-tDataCov   = nan(Nelc,Nelc,0);
-vScore     = [];
-vPowerMean = nan(Nelc,0);
-dimSubSpcMin = Nelc;
+% mData             = nan(D, 0);
+tDataCov            = nan(Nelc,Nelc,0);
+vScore              = [];
+vPowerMean          = nan(Nelc,0);
+dimSubSpcMin        = Nelc;
+vSessionOfCov       = [];
 for ii = 1 : Ns
     subject = vSubjectIdx(ii);
     
@@ -39,6 +40,8 @@ for ii = 1 : Ns
         load([dirPath, fileName]);
         disp(['Calculating for subject ' num2str(ii) ' of ' num2str(Ns) ...
             ', session ' num2str(ss)]);
+        
+        vSessionOfCov(end+1)  =   ss;
         
         mX          = data(vElectordeIdx,:,:);
         Nt          = size(mX, 3);
@@ -80,7 +83,7 @@ end
 disp('Done!');
 
 %% Eigenvecs
-kSubj = 6;
+kSubj = 8;
 kVec = 2;
 [mV,~]=eig(tDataCov(:,:,kSubj));
 figure;
@@ -89,6 +92,17 @@ scatterElectrodeMap(vElectordeIdx,mV(:,kVec))
 subplot(1,2,2);
 imagesc(mV); colorbar;
 
+%% Projection of covs to 2d subspace
+t2dCovs = nan(2,2,size(tDataCov,3));
+for ss = 1:size(tDataCov,3)
+    [mV,mD]           =   eig(tDataCov(:,:,ss));
+    mV1               =   inv(mV);
+    t2dCovs(:,:,ss)   =   mV(1:2,:)*mD*mV1(:,1:2);
+end
+mCovVecs=CovsToVecs(t2dCovs);
+scatter3(mCovVecs(1,:),mCovVecs(2,:),mCovVecs(3,:),...
+    10,vSessionOfCov,'Fill');
+colorbar;
 %% Extract Classifications from XLS
 vDHRS           = XlsFile(:,9)';
 depressionThres = 0.5;
