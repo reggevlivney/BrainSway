28e873a5ed3655099e23235fc8b08b70ad833ee8
reggevlivney <reggevlivney@campus.technion.ac.il>
2018-05-22 22:05:38 +0300

    Revert "PT code"
    This reverts commit 0702c0078530d7296e0868fe695fff5991c26f36.

--- PT_trial.m
+++ PT_trial.m
@@ -4,17 +4,18 @@ clear;
 % clc;
 
 %% Import Data
-%dirPath = 'C:\Users\Oryair\Desktop\Workarea\BrainSway\'; %Or's path
- dirPath = 'C:\Users\DELL\Desktop\Data for P4\'; %Reggev's path
-%dirPath = 'D:\BrainSwayData\';                  %Matan's Path
+dirPath = 'C:\Users\Oryair\Desktop\Workarea\BrainSway\'; %Or's path
+% dirPath = 'C:\Users\DELL\Desktop\Data for P4\'; %Reggev's path
+% dirPath = 'D:\BrainSwayData\';                  %Matan's Path
 XlsFile = xlsread([dirPath, 'clinicalHDRS-2.xlsx']);
 
 %% Preprocess data
-vSubjectIdx = XlsFile(:,1);        
-Nelc               = 10;  % Num of electrodes
+vSubjectIdx        = XlsFile(:,1);        
+Nelc               = 40;  % Num of electrodes
 vSessions          = 2:5;
 vElectordeIdx      = sort(randperm(62, Nelc));  % Pick random electrodes
 Ns                 = length(vSubjectIdx); %Number of subjects
+% Ns                 = 10; %Number of subjects
 vSubjectsInSession = vSubjectIdx;
 
 %% Create Cov matricies and Riemann averages
@@ -42,7 +43,7 @@ for ii = 1 : Ns
         load([dirPath, fileName]);
         disp(['Calculating for subject ' num2str(ii) ' of ' num2str(Ns) ...
             ', session ' num2str(ss)]);
-        mX          = data(vElectordeIdx,:,:);
+        mX          = data(vElectordeIdx,1200:end,:);
         Nt          = size(mX, 3);
         dimSubSpc    = Nelc;
         mXSubSpc     = mX;
@@ -50,21 +51,24 @@ for ii = 1 : Ns
             mDetails(end+1,:)     =   [ii,ss,hh];
             tDataCov(:,:,end+1)   =   cov(mXSubSpc(:,:,hh).');
         end
-        mTrialsMean(end+1,:)     =   [ii,ss];
-        idx                      =   find(mDetails(:,1) == ii & mDetails(:,2) == ss);
-        tTrialsMeanCov(:,:,end+1)    =   RiemannianMean(tDataCov(:,:,idx));
+        mTrialsMean(end+1,:)      = [ii,ss];
+        idx                       = find(mDetails(:,1) == ii & mDetails(:,2) == ss);
+        tTrialsMeanCov(:,:,end+1) = RiemannianMean(tDataCov(:,:,idx));
     end
 end
 disp('Done!');
+
 %% Riemannian Mean
 nSess           =   length(vSessions);
 tMeanCov        =   nan(Nelc,Nelc,nSess);
 vMeanDetails    =   nan(1,nSess);
-for ii = 1:nSess
-    ss                  =   vSessions(ii);
+% for ii = 1 : nSess
+for ii = 1 : Ns
+%     ss                  =   vSessions(ii);
+    ss                  =   ii;
     disp("Riemannian Mean over session " + num2str(ss));
     vMeanDetails(ii)    =   ss;
-    vSessIdx            =   find(mTrialsMean(:,2) == ss);
+    vSessIdx            =   find(mTrialsMean(:,1) == ss);
     tMeanCov(:,:,ii)    =   RiemannianMean(tTrialsMeanCov(:,:,vSessIdx));
 end
 
@@ -72,56 +76,47 @@ end
 mMeanMeanCov = RiemannianMean(tMeanCov);
 
 %% Parallel Transport
-tPTDataCov = nan(Nelc,Nelc,size(tTrialsMeanCov,3));
-for ii = 1:nSess
+% figure;
+
+tPTDataCov           = nan(Nelc,Nelc,size(tTrialsMeanCov,3));
+mMeanMinusSquareRoot = mMeanMeanCov^(-1/2);
+for ii = 1 : nSess
     ss = vSessions(ii);
-    E = (mMeanMeanCov/tMeanCov(:,:,ii))^0.5;
+    E = ( mMeanMeanCov / tMeanCov(:,:,ii) )^(1/2);
     vSessIdx            =   find(mTrialsMean(:,2)==ss);
-    for jj = vSessIdx.'
-        tPTDataCov(:,:,jj) = (E*tTrialsMeanCov(:,:,jj)*(E.'));
-        tPTDataCov(:,:,jj) = logm((mMeanMeanCov^(-0.5))*tPTDataCov(:,:,jj)*(mMeanMeanCov^(-0.5)));
+    for jj = vSessIdx'
+        tPTDataCov(:,:,jj) = E * tTrialsMeanCov(:,:,jj) * E.';
+%          = logm(mMeanMinusSquareRoot * M * mMeanMinusSquareRoot);
+%         stem((eig(M))); drawnow; pause(.4);
     end
 end
 
 %% Figures
 figure;
 subplot(1,2,2);
-mPTVecs = CovsToVecs(tPTDataCov).';
-coeff  = pca(mPTVecs);
-mDPTVecs = mPTVecs*coeff; 
+mPTVecs  = CovsToVecs(tPTDataCov).';
+[coeff, mDPTVecs] = pca(mPTVecs);
+% mDPTVecs = mPTVecs*coeff; 
 mDPTVecs = mDPTVecs(:,1:3);
-scatter3(mDPTVecs(:,1),mDPTVecs(:,2),mDPTVecs(:,3),20,mTrialsMean(:,2),'filled');
+scatter3(mDPTVecs(:,1), mDPTVecs(:,2), mDPTVecs(:,3), 100, mTrialsMean(:,2), 'Fill');
 title("Sessions Scattering: after PT and PCA 3");
+
 subplot(1,2,1);
 mVecs  = CovsToVecs(tTrialsMeanCov).';
-coeff  = pca(mVecs);
-mDVecs = mVecs*coeff; 
+[coeff, mDVecs] = pca(mVecs);
+% mDVecs = mVecs*coeff; 
 mDVecs = mDVecs(:,1:3);
-scatter3(mDVecs(:,1),mDVecs(:,2),mDVecs(:,3),20,mTrialsMean(:,2),'filled');
+scatter3(mDVecs(:,1), mDVecs(:,2), mDVecs(:,3), 100, mTrialsMean(:,2), 'Fill');
 title("Sessions Scattering: PCA 3");
 
+mC = lines(Ns);
 figure;
 subplot(1,2,2);
-scatter3(mDPTVecs(:,1),mDPTVecs(:,2),mDPTVecs(:,3),20,mTrialsMean(:,1),'filled');
+scatter3(mDPTVecs(:,1), mDPTVecs(:,2), mDPTVecs(:,3), 100, mC(mTrialsMean(:,1),:), 'Fill'); colorbar;
 title("Subjects Scattering: after PT and PCA 3");
 subplot(1,2,1);
-scatter3(mDVecs(:,1),mDVecs(:,2),mDVecs(:,3),20,mTrialsMean(:,1),'filled');
+scatter3(mDVecs(:,1), mDVecs(:,2), mDVecs(:,3), 100, mC(mTrialsMean(:,1),:), 'Fill'); colorbar;
 title("Subjects Scattering: PCA 3");
 
-%% Make Diffusion Map and Parallel Transport calculations
-%%% Create W distances matrix. Make sure to use only one session!
-Nmats   =   size(tPTDataCov,3);
-mDists  =   zeros(Nmats,Nmats);
-for ii = 1:Nmats
-    for jj = ii+1:Nmats
-        mDists(ii,jj) = RiemannianDist(tPTDataCov(:,:,ii),tPTDataCov(:,:,jj));
-        mDists(jj,ii) = mDists(ii,jj);
-    end
-end
-eDiff        = mean(mDists(mDists~=0));
-mDistsDiff   = exp(-mDists.^2/eDiff^2);
-
-[mVDiff,mDDiff] = eig(mDistsDiff);
-
-mtSNE           = mVDiff*mDDiff;
-mtSNE           = TSNE(mtSNE,vScore,3,Nmats);
+%%
+Data = [mDPTVecs, mTrialsMean(:,2)];
