b16059f638c5c5ded0c6815bf236af0939f048c8
Matanal <Matanal@campus.technion.ac.il>
2018-06-16 00:10:56 +0300

    Orginized Better
    Or maybe not?

--- ProjectCode/main.m
+++ ProjectCode/main.m
@@ -1,8 +1,15 @@
+addpath('RiemannianTools');
+addpath('CovCalcs');
+addpath('ReductionExtraction');
+addpath('CovData');
+
 %% Calculation of covariance;
 % CreateCov;
+
 
 %% Calculation of the covariances means - Riemannian Mean
 % MeanCovs;
 
 %% Operating Diffusion Maps
 DiffMapCov;
+
