00afd5b8eca28316ce3e7186d6a89f43d8f8d2bf
reggevlivney <reggevlivney@campus.technion.ac.il>
2018-06-16 14:22:55 +0300

    Removed the ignoring of points in diffmap

--- ProjectCode/ReductionExtraction/DiffMapCov.m
+++ ProjectCode/ReductionExtraction/DiffMapCov.m
@@ -2,9 +2,6 @@
 load('meanCovs');
 tMeanCovEX          = tMeanCov;
 vScoreEX            = vScore;
-tMeanCovEX(:,:,89)  = []; % Remember to erase this later!
-vScoreEX(89,:)      = []; % ditto
 [ diffusion_matrix, diffusion_eig_vals , axes] = Diff_map(tMeanCovEX, vScoreEX(:,1).',vScoreEX(:,3).',vAxes);
-
 hlink = linkprop(axes,{'CameraPosition','CameraUpVector'});
 rotate3d on
